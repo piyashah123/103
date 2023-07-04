9c2ca3c90b95097f06204315f31520c28277f8e7
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2023-07-04 15:57:39 +0530

    Add files via upload

---
+++ DownloadAndMove.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
+
+from_dir = "D:/Downloads"
+to_dir = "D:/Downloads/Downloaded_Files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        name,extension=os.path.splitext(event.src_path)
+        for key,value in dir_tree.items():
+            if extension in value:
+                file_name=os.path.basename(event.src_path)
+                print("Downloaded+file_name")
+                path1=from_dir+'/+file_name'
+                path2=
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+while True:
+    time.sleep(2)
+    print("running...")
+
+    
